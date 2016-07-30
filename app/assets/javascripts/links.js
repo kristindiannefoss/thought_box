062b23262d91e1485390f66f88d2353949866a7f
Kris Foss <kris.foss@gmail.com>
2016-07-30 12:52:03 -0600

    been-there class persists, HAZZAH

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -16,6 +16,18 @@ function loadLinks(){
 }
 
 function linkHTML(link){
+  if(link.read === true){
+    return (
+      "<tr data-link-id=" + link.id + " class='been-there'>" +
+      "<td contenteditable='true' data-link-id=" + link.id +
+      "class='title-input'>" + link.title + "</td>" +
+      "<td contenteditable='true' class='url-input'>" + link.url + "</td>" +
+      "<td>" + link.read + "<td>" +
+      "<button class='btn btn-default fake-link' data-link-id='" + link.id +
+      "' data-user-id='" + link.user_id + "'>Mark as Read</button></td>" +
+      "</tr>"
+    );
+  } else {
     return (
       "<tr data-link-id=" + link.id + ">" +
       "<td contenteditable='true' data-link-id=" + link.id +
@@ -25,7 +37,8 @@ function linkHTML(link){
       "<button class='btn btn-default fake-link' data-link-id='" + link.id +
       "' data-user-id='" + link.user_id + "'>Mark as Read</button></td>" +
       "</tr>"
-  );
+    );
+  }
 }
 
 $(document).on('click', '.fake-link', function(e){
