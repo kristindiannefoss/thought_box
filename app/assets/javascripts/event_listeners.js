ac7bcd62b9293c341dd9ca030c3c8f1a0f635037
Kris Foss <kris.foss@gmail.com>
2016-07-30 14:10:56 -0600

    get inner text of e

--- app/assets/javascripts/event_listeners.js
+++ app/assets/javascripts/event_listeners.js
@@ -16,13 +16,15 @@ function updateLink(e){
 
 $(document).on('click', '.title-input', function(e){
   console.log(e.target.dataset.linkId);
+  console.log(e.target.innerText);
+  debugger;
   e.target.setAttribute("contenteditable", "true");
 
   // $.ajax({
   //   method: 'PATCH',
   //   url: "api/v1/links/" + e.target.dataset.linkId,
   //   dataType: "json",
-  //   data: {link: {id: e.target.dataset.linkId, read: true, user_id: e.target.dataset.userId}},
+  //   data: {link: {id: e.target.dataset.linkId, title: e.target.val(), user_id: e.target.dataset.userId}},
   //   success: function(link){
   //     console.log(e.target.dataset.linkId);
   //     $("tr[data-link-id='" + e.target.dataset.linkId + "']").addClass("been-there");
