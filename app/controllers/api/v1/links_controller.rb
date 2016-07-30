9c8bb916ccfac2d17cd919d7f478190b3444865c
Kris Foss <kris.foss@gmail.com>
2016-07-30 12:55:55 -0600

    split into files for readability, need to remove read attr from dom

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,15 +1,12 @@
 class Api::V1::LinksController < Api::ApiController
   respond_to :json
-  # before_action :set_user
 
   def index
-    # binding.pry
     @user = current_user
     respond_with @user.links
   end
 
   def show
-    # @user = current_user
     @user = User.find(params[:id])
     respond_with @user.links.find(params[:link][:id])
   end
@@ -28,25 +25,13 @@ class Api::V1::LinksController < Api::ApiController
   end
 
   def update
-    # byebug;
-    # @user = User.find(params[:id])
     @link = Link.find(params[:link][:id])
     respond_with @link.update(link_params)
-    # @link = Link.find(params[:id])
-    # respond_with @link if @link.update(link_params)
   end
 
   private
-
-    def set_user
-      # @user = User.find(params[:id])
-        @user = current_user
-    end
-
 
     def link_params
       params.require(:link).permit(:id, :title, :url, :read, :user_id)
     end
 end
-
-# {title: "Stuff", url: "www.deepthoughtsbyjackhandey.com/"}
